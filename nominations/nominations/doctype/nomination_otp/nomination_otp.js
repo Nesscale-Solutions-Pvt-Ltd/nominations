3ca72da4dcc744c6a2e77b402057659e6ecb1450
Bhavesh Maheshwari <34086262+bhavesh95863@users.noreply.github.com>
2026-05-19 12:04:01 +0530

    feat: add Nomination OTP doctype with initial structure and test cases

---
+++ nominations/nominations/doctype/nomination_otp/nomination_otp.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Nesscale Solutions Pvt Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Nomination OTP", {
+// 	refresh(frm) {
+
+// 	},
+// });
